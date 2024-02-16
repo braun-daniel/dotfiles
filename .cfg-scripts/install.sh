787e1f9de6fbd5a9376e5ea7b5596ab1c068f374
Daniel Braun <7925710+braun-daniel@users.noreply.github.com>
2024-02-16 21:48:22 +0100

    Added macports support;

--- .cfg-scripts/install.sh
+++ .cfg-scripts/install.sh
@@ -1,22 +1,32 @@
 #!/bin/zsh
 # See https://www.atlassian.com/git/tutorials/dotfiles
 
-# Brew repos
-brew tap -q sdkman/tap
+# Check if MacPorts is installed
+if command -v brew >/dev/null 2>&1; then
+  package_manager="homebrew"
+elif command -v port >/dev/null 2>&1; then
+  package_manager="macports"
+else
+  echo "Neither MacPorts nor Homebrew is installed. Please install either of them."
+  exit 1
+fi
 
-# Install required packages
-brew update && brew install -q \
-  sdkman-cli \
-  fzf \
-  thefuck \
-  bat \
-  fd \
-  yt-dlp \
-  eza \
-  diff-so-fancy \
-  z \
-  autojump \
-  navi
+# Check if the package manager can be overridden with an environment variable
+if [ -n "$PACKAGE_MANAGER" ]; then
+  package_manager="$PACKAGE_MANAGER"
+fi
+
+# Define the list of packages
+packages="sdkman fzf thefuck bat fd yt-dlp eza diff-so-fancy z autojump navi"
+
+# Install required packages using the selected package manager
+if [ "$package_manager" = "macports" ]; then
+  sudo port selfupdate
+  sudo port install -q $packages
+elif [ "$package_manager" = "homebrew" ]; then
+  brew update
+  brew install -q $packages
+fi
 
 # Check out GIT repos
 mkdir -p $HOME/.cfg
